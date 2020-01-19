b339adc5c9808a7a7f32a884bbabffe8aa68ddcd
wangsw <wangsw@ushareit.com>
2020-01-19 23:06:00 +0800

    remove router

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 // 载入默认全局样式 normalize
 import '@alifd/next/reset.scss';
-import router from './router';
+// import router from './router';
+import HomePage from './pages/HomePage/index';
 
 const ICE_CONTAINER = document.getElementById('ice-container');
 
@@ -9,4 +10,4 @@ if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
 
-ReactDOM.render(router(), ICE_CONTAINER);
+ReactDOM.render(HomePage(),ICE_CONTAINER);
