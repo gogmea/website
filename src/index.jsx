b670f68ddfb6ef6f2cf8abd05bb4803f071107f1
wangsw <wangsw@ushareit.com>
2020-01-20 21:42:31 +0800

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 // 载入默认全局样式 normalize
-import '@alifd/next/reset.scss';
-// import router from './router';
-import HomePage from './pages/HomePage/index';
+import "@alifd/next/reset.scss";
+import router from "./router";
 
-const ICE_CONTAINER = document.getElementById('ice-container');
+const ICE_CONTAINER = document.getElementById("ice-container");
 
 if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
 
-ReactDOM.render(HomePage(),ICE_CONTAINER);
+ReactDOM.render(router(), ICE_CONTAINER);
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import Banner from './components/Banner';
-import Introduction from './components/Introduction';
-import Feature from './components/Feature';
-import Data from './components/Data';
-import Solution from './components/Solution';
-import Resource from './components/Resource';
-import ComplexDialog from './components/ComplexDialog';
+import React from "react";
+import Banner from "./components/Banner";
+import Introduction from "./components/Introduction";
+import Feature from "./components/Feature";
+import Data from "./components/Data";
+import Solution from "./components/Solution";
+import Resource from "./components/Resource";
+import ComplexDialog from "./components/ComplexDialog";
 
 export default function HomePage() {
   return (
     <div>
       <Banner />
       <Resource />
-
       {/* <Introduction /> */}
       <Feature />
       {/* <Data /> */}
       {/* <Solution /> */}
-      <ComplexDialog/>
+      <ComplexDialog />
     </div>
   );
 }
