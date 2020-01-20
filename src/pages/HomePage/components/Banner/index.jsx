fa1dab9e614c338bab98af9ce1f46374bc8c8530
wangsw <wangsw@ushareit.com>
2020-01-20 21:33:02 +0800

    fix

--- src/pages/HomePage/components/Banner/index.jsx
+++ src/pages/HomePage/components/Banner/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import styles from './index.module.scss';
-import href from '../../const';
+import React from "react";
+import styles from "./index.module.scss";
+import href from "../../const";
 
 export default function Banner() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.title}>西柚宝贝，喜爱宝贝。</div>
-        <div className={styles.desc}>和宝宝一起成长，记录每个幸福瞬间。</div>
-        <a className={styles.link} href={href}>下载应用</a>
+        <div className={styles.title}>西柚宝贝，不负成长每一天。</div>
+        <div className={styles.desc}>记录时光，见证成长。</div>
+        <a className={styles.link} href={href}>
+          下载应用
+        </a>
       </div>
     </div>
   );
